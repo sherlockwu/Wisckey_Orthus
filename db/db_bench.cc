bbebbcede16dee59ed74016012b6f4669be72d21
kanwu.wisc@gmail.com <kanwu.wisc@gmail.com>
2020-04-04 17:49:43 -0500

    start to implement cache for vlog

--- db/db_bench.cc
+++ db/db_bench.cc
@@ -732,7 +732,7 @@ class Benchmark {
     
     //Kan: for persist cache
     options.use_persist_cache = true;
-    options.persist_block_cache = NewLRUCache(((size_t)0)*1024*1024*1024);
+    options.persist_block_cache = NewPersistLRUCache(((size_t)1)*1024*1024*1024);
 
     Status s = DB::Open(options, FLAGS_db, &db_);
     if (!s.ok()) {
